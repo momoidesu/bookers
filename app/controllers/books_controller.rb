8e7d013558417d14388ea54ec7b6fd1279045342
EC2 Default User <ec2-user@ip-172-31-35-140.ap-northeast-1.compute.internal>
2023-12-14 12:06:15 +0000

    一時休止

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,23 +5,39 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    books.save
-    redirect_to '/top'
+    book.save
+    redirect_to book_path(book)
   end
 
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
-  
+
+  def destroy
+     book = Book.find(params[:id])
+     book.destroy
+     redirect_to books_path
+  end
+
+  def update
+     book = Book.find(params[:id])
+     book.update(book_params)
+     redirect_to book_path(book.id)
+  end
+
   private
-  def book_params
-    params.require(:list).permit(:title, :body)
-  end
+     def book_params
+     params.require(:book).permit(:title, :body)
+     end
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get '/top' => 'homes#top'
-  post 'books' => 'books#create'
+  # get 'books/new'
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/edit'
+  # get '/top' => 'homes#top'
+  # post 'books' => 'books#create'
+  # get 'books' => 'books#index'
+  # get 'books/:id' => 'books#show'
+  resources :books 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
